64b2b06b6abea81e036311f09f05e767befd283c
teeteathao <87237312+teeteathao@users.noreply.github.com>
2021-10-04 15:56:11 -0700

    moved start button and added submit button

--- Assets/script.js
+++ Assets/script.js
@@ -1,16 +1,19 @@
 //Variables
-var startButton = document.getElementById("startQuizBtn");
+var startButton = document.getElementById("startBtn");
+var submitButton = document.getElementById("submitBtn");
 
-// Add event listener to Start Quiz button
+// Add event listener to buttons
 startButton.addEventListener("click", startQuiz);
+submitButton.addEventListener("click", submitAnswer);
+
 
 function startQuiz(){
-    console.log("startQuiz")
+    console.log("start")
 
 }
 
-function setNextQuestion(){
-
+function submitAnswer(){
+    console.log("submit")
 
 }
 
@@ -18,3 +21,6 @@ function selectAnswer(){
 
 }
 
+const questions = {
+
+}
