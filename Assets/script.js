49635eeec3057c5939ef97e36db5c925a1c1d4d7
teeteathao <87237312+teeteathao@users.noreply.github.com>
2021-10-04 12:42:55 -0700

    Structure and made outline to html css and js

---
+++ Assets/script.js
@@ -0,0 +1,20 @@
+//Variables
+var startButton = document.getElementById("startQuizBtn");
+
+// Add event listener to Start Quiz button
+startButton.addEventListener("click", startQuiz);
+
+function startQuiz(){
+    console.log("startQuiz")
+
+}
+
+function setNextQuestion(){
+
+
+}
+
+function selectAnswer(){
+
+}
+
