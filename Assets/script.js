e61fce93f5df5c2e192a16395663f8b4b90e8a0c
teeteathao <87237312+teeteathao@users.noreply.github.com>
2021-10-09 10:24:24 -0700

    add timer and structure for the quiz

--- Assets/script.js
+++ Assets/script.js
@@ -1,26 +1,78 @@
 //Variables
 var startButton = document.getElementById("startBtn");
 var submitButton = document.getElementById("submitBtn");
+var time = document.getElementById("time")
 
+var timerId;
+var timerCountdown = 60;
+
+const questions = [{
+    title: "What do object properties consists of?",
+    choices: ["key value pair", "list", "var", "food"],
+    answer: "key value pair",
+},
+{
+    title: "What do object properties consists of?",
+    choices: ["key value pair", "list", "var", "food"],
+    answer: "key value pair",
+},
+{
+    title: "What do object properties consists of?",
+    choices: ["key value pair", "list", "var", "food"],
+    answer: "key value pair",
+},
+{
+    title: "What do object properties consists of?",
+    choices: ["key value pair", "list", "var", "food"],
+    answer: "key value pair",
+},
+{
+    title: "What do object properties consists of?",
+    choices: ["key value pair", "list", "var", "food"],
+    answer: "key value pair",
+},
+]
 // Add event listener to buttons
 startButton.addEventListener("click", startQuiz);
 submitButton.addEventListener("click", submitAnswer);
 
 
+// Start quiz
 function startQuiz(){
-    console.log("start")
-
+    // console.log("start")
+    timerId = setInterval(timer, 1000);
 }
 
-function submitAnswer(){
-    console.log("submit")
-
+function timer(){
+    timerCountdown--;
+    time.textContent = timerCountdown;
+    // console.log(timerCountdown)
+    if (timerCountdown <= 0){
+        endQuiz();
+    }
 }
 
-function selectAnswer(){
-
+function endQuiz(){
+    clearInterval(timerId);
 }
 
-const questions = {
 
+
+function getQuestion(){
+    // declare index 0 
+    // create var to title on the page
+    // loop (for each) over choices, create dynamic button using js
+    // creat onclick event question click
+    // display to page
+}
+
+function questionClick(){
+    // condition if answer is correct/incorrect
+    // if incorrect subtract time
+    // display new time on page
+    // move to next question
+}
+
+function saveHighScore(){
+    // local storage
 }
