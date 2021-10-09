25560553706dcdcfb7c9fbd74c4c6cde636b6b7d
teeteathao <87237312+teeteathao@users.noreply.github.com>
2021-10-09 11:08:51 -0700

    changed html body and added questions

--- Assets/script.js
+++ Assets/script.js
@@ -8,28 +8,28 @@ var timerCountdown = 60;
 
 const questions = [{
     title: "What do object properties consists of?",
-    choices: ["key value pair", "list", "var", "food"],
-    answer: "key value pair",
+    choices: ["Key Value Pair", "List", "Variables", "Elements"],
+    answer: "Key Value Pair",
 },
 {
-    title: "What do object properties consists of?",
-    choices: ["key value pair", "list", "var", "food"],
-    answer: "key value pair",
+    title: "What is === operator?",
+    choices: ["Strict Equality", "Super Equal", "Triple Equal", "Sort of Equal"],
+    answer: "Strict Equality",
 },
 {
-    title: "What do object properties consists of?",
-    choices: ["key value pair", "list", "var", "food"],
-    answer: "key value pair",
+    title: "What is the HTML tag under which one can write the JavaScript code?",
+    choices: ["<javascript>", "<script>", "<js>", "<java>"],
+    answer: "<script>",
 },
 {
-    title: "What do object properties consists of?",
-    choices: ["key value pair", "list", "var", "food"],
-    answer: "key value pair",
+    title: "What surrounds a string?",
+    choices: ["Quotes", "Parentheses", "Curly Brackets", "Square Brackets"],
+    answer: "Quotes",
 },
 {
-    title: "What do object properties consists of?",
-    choices: ["key value pair", "list", "var", "food"],
-    answer: "key value pair",
+    title: "The conditional statement, in Javascript, begins with the word _______.",
+    choices: ["Else", "Then", "If", "Start"],
+    answer: "If",
 },
 ]
 // Add event listener to buttons
@@ -43,6 +43,7 @@ function startQuiz(){
     timerId = setInterval(timer, 1000);
 }
 
+//timer starts when user pushes start
 function timer(){
     timerCountdown--;
     time.textContent = timerCountdown;
@@ -52,11 +53,10 @@ function timer(){
     }
 }
 
+//end quiz when the timer reaches 0
 function endQuiz(){
     clearInterval(timerId);
 }
-
-
 
 function getQuestion(){
     // declare index 0 
